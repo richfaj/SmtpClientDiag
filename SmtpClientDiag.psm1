6191f6d2f0f0a242f1dc42d4dbfc23f982d29777
Richard Fajardo <richfaj@microsoft.com>
2023-06-16 09:51:42 -0500

    Added feature to test auth blob for common issues

--- SmtpClientDiag.psm1
+++ SmtpClientDiag.psm1
@@ -1,7 +1,7 @@
 <#
 MIT License
 
-Copyright (c) 2022 Richard Fajardo
+Copyright (c) 2023 Richard Fajardo
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -289,7 +289,6 @@ function Test-SmtpClientSubmission() {
         WriteFile
     }
 }
-
 function Test-ConnectorAttribution()
 {
     param(
@@ -390,7 +389,200 @@ function Test-ConnectorAttribution()
         WriteFile
     }
 }
+function Test-SmtpSaslAuthBlob() {
+    param(
+        [CmdletBinding()]
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]$EncodedAuthBlob
+    )
 
+    $Script:BlobResult = [PSCustomObject] @{
+        AuthBlobUserName   = $null
+        AuthBlobToken      = $null
+        OAuthTokenAudience = $null
+        OAuthTokenScopes   = $null
+        OAuthTokenRoles    = $null
+        OAuthTokenUpn      = $null
+        IsAuthBlobValid    = $false
+        IsAuthTokenValid   = $false
+    }
+
+    # Write error if no auth blob is provided instead of making parameter mandatory.
+    # Making it mandatory would truncate the string if user provided after execution
+    if ([string]::IsNullOrEmpty($EncodedAuthBlob)) {
+        Write-Error "AuthBlob is null or empty. Please supply a value to test." -ErrorAction Stop
+    }
+
+    $decodedAuthBlob = $null
+
+    try {
+        $decodedAuthBlob = DecodeBase64Value($EncodedAuthBlob)
+    }
+    catch {
+        Write-Verbose "Failed to decode auth blob: $EncodedAuthBlob"
+        Write-Error "AuthBlob is not valid base64 encoded string. Check your input and try again." -ErrorAction Stop
+    }
+
+    [char[]]$charArray = $decodedAuthBlob.ToCharArray()
+    [int]$userIndex = $decodedAuthBlob.IndexOf("user=")
+    [int]$authIndex = $decodedAuthBlob.IndexOf("auth=")
+    [int]$bearerIndex = $decodedAuthBlob.IndexOf("Bearer ")
+    [char]$ctrlA = [char]0x01
+
+    # Validate that auth blob is correctly formatted
+    # Example: base64("user=" + userName + "^Aauth=Bearer " + accessToken + "^A^A")
+    # ^A represents a Control + A (%x01) character
+
+    # Check if 'user' is present at the beginning of the string
+    if ($userIndex -ne 0) {
+        Write-Verbose "Invalid Authblob. Expected 'user' at index 0. IndexOf('user='):$userIndex"
+        Write-Error "Authblob is incorrectly formatted. User not found." -ErrorAction Stop
+    }
+
+    if ($authIndex -eq -1) {
+        Write-Verbose "Invalid Authblob. 'auth' not found in auth blob."
+        Write-Error "Authblob is incorrectly formatted. Auth not found." -ErrorAction Stop
+    }
+
+    if ($bearerIndex -eq -1 -or $bearerIndex -lt $authIndex) {
+        Write-Verbose "Invalid Authblob. 'Bearer' not found in auth blob or not in correct position. IndexOf('Bearer '):$bearerIndex"
+        Write-Error "Authblob is incorrectly formatted. Bearer not found." -ErrorAction Stop
+    }
+
+    # Check if CTRL-A character is present before auth
+    if ($charArray[$authIndex - 1] -ne $ctrlA) {
+        $Script:BlobResult | Format-List
+        $charHex = GetCharHexValue($charArray[$authIndex - 2])
+        Write-Verbose "Invalid Authblob. Expected ascii character 0x01 before 'auth' but found char '0x$charHex' at index '$authIndex'."
+        Write-Error "Authblob is incorreclty formatted. Missing CTRL-A character." -ErrorAction Stop
+    }
+
+    # Check if CTRL-A is present at the end of the string
+    if ($charArray[-1] -ne $ctrlA -and $charArray[-2] -ne $ctrlA) {
+        $Script:BlobResult | Format-List
+        Write-Verbose "Invalid Authblob. Expected ascii character 0x01 0x01 at end of string but found '$($charArray[-1])$($charArray[-2])'."
+        Write-Error "Authblob is incorreclty formatted. Missing CTRL-A character." -ErrorAction Stop
+    }
+
+    $Script:BlobResult.AuthBlobUserName = $decodedAuthBlob.Substring($userIndex + 5, $authIndex - 6)
+    $Script:BlobResult.AuthBlobToken = $decodedAuthBlob.Substring($authIndex + 5, $decodedAuthBlob.Length - $authIndex - 7).Replace("Bearer ", "")
+
+    if ([string]::IsNullOrEmpty($Script:BlobResult.AuthBlobUserName)) {
+        $Script:BlobResult | Format-List
+        Write-Verbose "Found 'user' in auth blob but it contains no value."
+        Write-Error "AuthBlob does not contain a user" -ErrorAction Stop
+    }
+
+    if ([string]::IsNullOrEmpty($Script:BlobResult.AuthBlobToken)) {
+        $Script:BlobResult | Format-List
+        Write-Verbose "Found 'auth' in auth blob but it contains no value."
+        Write-Error "AuthBlob does not contain a token" -ErrorAction Stop
+    }
+
+    try {
+        CheckAccessToken($Script:BlobResult.AuthBlobToken)
+    }
+    catch {
+        $Script:BlobResult | Format-List
+        Write-Error $_.Exception
+        Write-Error "Token in auth blob is invalid." -ErrorAction Stop
+    }
+
+    # If we get here, we're good
+    $Script:BlobResult.IsAuthBlobValid = $true
+    $Script:BlobResult | Format-List
+}
+
+function DecodeBase64Value([string] $value) {
+    if ([string]::IsNullOrEmpty($value)) {
+        return $null
+    }
+    $valueBytes = [System.Convert]::FromBase64String($value)
+    return [System.Text.Encoding]::UTF8.GetString($valueBytes)
+}
+
+function GetCharHexValue([char] $char) {
+    return ('{0:x}' -f [int]$char).ToUpper()
+}
+
+function CheckAccessToken($encodedToken) {
+    if ([string]::IsNullOrEmpty($encodedToken)) {
+        Write-Verbose "Token is null or empty. Skipping token verfication."
+        $Script:BlobResult.IsAuthTokenValid = $false
+        return
+    }
+
+    [bool]$tokenValid = $true
+    [string]$decodedToken = $null
+    [object]$token = $null
+    [bool]$isAppAuth = $false
+    [string[]]$tokenParts = $encodedToken.Split(".")
+
+    # Token should have header, payload and signature
+    if ($tokenParts.Count -ne 3 -or [string]::IsNullOrEmpty($tokenParts[0]) -or [string]::IsNullOrEmpty($tokenParts[1])) {
+        throw "Invalid token. Token header or payload is null or empty."
+    }
+
+    # Can header and payload be decoded? Throws exception if not
+    try {
+        DecodeBase64Value($tokenParts[0]) | Out-Null
+        $decodedToken = DecodeBase64Value($tokenParts[1])
+        $token = ConvertFrom-Json $decodedToken
+    }
+    catch {
+        throw "Failed to decode authentication token or token is not valid JSON."
+    }
+
+    $Script:BlobResult.OAuthTokenAudience = $token.aud
+    $Script:BlobResult.OAuthTokenUpn = $token.upn
+    $Script:BlobResult.OAuthTokenScopes = $token.scp
+    $Script:BlobResult.OAuthTokenRoles = $token.roles
+
+    if (-not [string]::IsNullOrEmpty($token)) {
+        # Check for correct audience claim
+        if ($token.aud -ne "https://outlook.office365.com" -and $token.aud -ne "https://outlook.office.com") {
+            $tokenValid = $false
+            Write-Verbose "Unexpected audience claim. Expected 'https://outlook.office365.com' or 'https://outlook.office.com' but found '$($token.aud)'."
+            Write-Warning "Authentication token contains an invalid audience claim for SMTP Client Submission."
+        }
+
+        # Likely client credential flow if UPN claim is null or empty
+        if ([string]::IsNullOrEmpty($token.upn)) {
+            $isAppAuth = $true
+            Write-Verbose "UPN claim is null or empty."
+            Write-Warning "Application authentication detected. UPN claim not found in token."
+        }
+
+        # If using client credential flow check the roles claim
+        if ($isAppAuth) {
+            Write-Verbose "Checking roles claim for SMTP.SendAsApp permission."
+            $permissions = $token.roles.Split()
+            if (-not $permissions.Contains("SMTP.SendAsApp")) {
+                $tokenValid = $false
+                Write-Verbose "Not checking scopes claim as this is not user authentication."
+                Write-Verbose "Invalid role in token. Expected 'SMTP.Send' but found $($token.scp)"
+                Write-Warning "Required permission for SMTP Client Submission not found in token."
+            }
+        }
+
+        # Else check the scopes claim
+        else {
+            Write-Verbose "Not checking roles claim as this is not application authentication."
+            Write-Verbose "Checking scopes claim for SMTP.Send permission."
+            $permissions = $token.scp.Split()
+            if (-not $permissions.Contains("SMTP.Send")) {
+                $tokenValid = $false
+                Write-Verbose "Invalid scope in token. Expected 'SMTP.Send' but found $($token.scp)"
+                Write-Warning "Required permission for SMTP Client Submission not found in token."
+            }
+        }
+    }
+    else {
+        $tokenValid = $false
+    }
+
+    $Script:BlobResult.IsAuthTokenValid = $tokenValid
+}
 function RetrieveCertificateFromCertStore($thumbprint) {
     $cert = Get-ChildItem -Path "cert:\LocalMachine\My" | Where-Object {$_.Thumbprint -eq $thumbprint}
 
@@ -590,7 +782,7 @@ function ContainsCapability([string] $c) {
 function AuthLogin() {
     if (ContainsCapability("AUTH LOGIN")) {
         SmtpCmd("AUTH LOGIN")
-
+        # TODO: Consider using DecodeBase64Value() function
         if ($Script:responseCode -eq 334) {
             $message = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($Script:smtpResponse.Substring(4))).ToLower()
             if ($message -eq "username:") {
